ce4f6b2020e3378399c7fe4f5729fda520348e5d
Chris Whitlam <chris.whitlam@infinityworks.com>
2021-06-14 19:39:59 +0100

    Remove unneeded packages

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
     }
   },
   moduleFileExtensions: ['ts', 'tsx', 'js'],
-  testPathIgnorePatterns: ['packages/api/build'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
@@ -16,7 +15,4 @@ module.exports = {
   reporters: ['default', 'jest-junit'],
   coverageDirectory: './coverage',
   coveragePathIgnorePatterns: ['/node_modules/'],
-  //globalSetup: '<rootDir>/jest.setup.js',
-  //globalTeardown: '<rootDir>/jest.teardown.js',
-  //maxWorkers: parseInt(process.env.JEST_MAX_WORKERS) || 2
 };
